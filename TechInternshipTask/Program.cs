a048f2af4e8c7f0f362b4cbd80cb570371027b3d
ldidi <dominila.l@gmail.com>
2021-05-15 16:04:02 +0200

    Create simply logic

--- TechInternshipTask/Program.cs
+++ TechInternshipTask/Program.cs
@@ -6,7 +6,49 @@ namespace TechInternshipTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                //var string1 = args[0].Split('.', '-');
+                //var string2 = args[1].Split('.', '-');
+
+
+                var string1 = Console.ReadLine().Split('.', '-');
+                var string2 = Console.ReadLine().Split('.', '-');
+
+
+
+                if (string1[2] != string2[2])
+                {
+                    Console.WriteLine( string1[0]+"."+string1[1]+"."+ string1[2]+"-" + string2[0] + "." + string2[1] + "." + string2[2]);
+                }
+
+                else
+                {
+                    if (string1[1] != string2[1])
+                    {
+                        Console.WriteLine(string1[0] + "." + string1[1] + "-" + string2[0] + "." + string2[1] + "." + string2[2]);
+                    }
+                    else
+                    {
+                        if (string1[0] != string2[0])
+                        {
+                            Console.WriteLine(string1[0] +  "-" + string2[0] + "." + string2[1] + "." + string2[2]);
+                        }
+                        else
+                        {
+                            Console.WriteLine(string2[0] + "." + string2[1] + "." + string2[2]);
+                        }
+
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+           
+          
         }
     }
 }
