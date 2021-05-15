b084b5fa8471b8a80a8fe23b5ae92106421c644a
ldidi <dominila.l@gmail.com>
2021-05-15 16:54:20 +0200

    Add data validation

--- TechInternshipTask/DataRageCreator.cs
+++ TechInternshipTask/DataRageCreator.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TechInternshipTask
 {
@@ -12,14 +10,25 @@ namespace TechInternshipTask
 
         public DataRageCreator(string str1, string str2)
         {
-            string1 = str1.Split('.', '-');
-            string2 = str2.Split('.', '-');
+
+            if (str1.Split('.', '-').Length == 3 && str2.Split('.', '-').Length == 3)
+            {
+                string1 = str1.Split('.', '-');
+                string2 = str2.Split('.', '-');
+            }
+            else
+            {
+                Console.WriteLine("ERROR - wrong input data ");
+                System.Environment.Exit(0);
+            }
+
         }
 
         public string GetPrintableData()
         {
             if (string1[2] != string2[2])
             {
+                if (Int32.Parse(string1[2]) > Int32.Parse(string2[2])) Swap();
                 return (string1[0] + "." + string1[1] + "." + string1[2] + "-" + string2[0] + "." + string2[1] + "." + string2[2]);
             }
 
@@ -27,12 +36,14 @@ namespace TechInternshipTask
             {
                 if (string1[1] != string2[1])
                 {
+                    if (Int32.Parse(string1[1]) > Int32.Parse(string2[1])) Swap();
                     return (string1[0] + "." + string1[1] + "-" + string2[0] + "." + string2[1] + "." + string2[2]);
                 }
                 else
                 {
                     if (string1[0] != string2[0])
                     {
+                        if (Int32.Parse(string1[0]) > Int32.Parse(string2[0])) Swap();
                         return string1[0] + "-" + string2[0] + "." + string2[1] + "." + string2[2];
                     }
                     else
@@ -43,5 +54,12 @@ namespace TechInternshipTask
             }
         }
 
+        private void Swap()
+        {
+            var tempStr = string1;
+            string1 = string2;
+            string2 = tempStr;
+
+        }
     }
 }
--- TechInternshipTask/Program.cs
+++ TechInternshipTask/Program.cs
@@ -6,7 +6,7 @@ namespace TechInternshipTask
     {
         static void Main(string[] args)
         {
-            var data =new DataRageCreator("01.03.2012", "03.04.2012");
+            var data = new DataRageCreator("01.03.2012", "03.04.2015");
             Console.WriteLine(data.GetPrintableData());
 
         }
