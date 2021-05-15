727de83714b3d71ace50065a777f1ec21d636969
ldidi <dominila.l@gmail.com>
2021-05-15 16:32:10 +0200

    Create DataRageCreator Class

---
+++ TechInternshipTask/DataRageCreator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechInternshipTask
+{
+    class DataRageCreator
+    {
+        string[] string1 = new string[3];
+        string[] string2 = new string[3];
+
+
+        public DataRageCreator(string str1, string str2)
+        {
+            string1 = str1.Split('.', '-');
+            string2 = str2.Split('.', '-');
+        }
+
+        public string GetPrintableData()
+        {
+            if (string1[2] != string2[2])
+            {
+                return (string1[0] + "." + string1[1] + "." + string1[2] + "-" + string2[0] + "." + string2[1] + "." + string2[2]);
+            }
+
+            else
+            {
+                if (string1[1] != string2[1])
+                {
+                    return (string1[0] + "." + string1[1] + "-" + string2[0] + "." + string2[1] + "." + string2[2]);
+                }
+                else
+                {
+                    if (string1[0] != string2[0])
+                    {
+                        return string1[0] + "-" + string2[0] + "." + string2[1] + "." + string2[2];
+                    }
+                    else
+                    {
+                        return string2[0] + "." + string2[1] + "." + string2[2];
+                    }
+                }
+            }
+        }
+
+    }
+}
--- TechInternshipTask/Program.cs
+++ TechInternshipTask/Program.cs
@@ -6,49 +6,9 @@ namespace TechInternshipTask
     {
         static void Main(string[] args)
         {
+            var data =new DataRageCreator("01.03.2012", "03.04.2012");
+            Console.WriteLine(data.GetPrintableData());
 
-            try
-            {
-                //var string1 = args[0].Split('.', '-');
-                //var string2 = args[1].Split('.', '-');
-
-
-                var string1 = Console.ReadLine().Split('.', '-');
-                var string2 = Console.ReadLine().Split('.', '-');
-
-
-
-                if (string1[2] != string2[2])
-                {
-                    Console.WriteLine( string1[0]+"."+string1[1]+"."+ string1[2]+"-" + string2[0] + "." + string2[1] + "." + string2[2]);
-                }
-
-                else
-                {
-                    if (string1[1] != string2[1])
-                    {
-                        Console.WriteLine(string1[0] + "." + string1[1] + "-" + string2[0] + "." + string2[1] + "." + string2[2]);
-                    }
-                    else
-                    {
-                        if (string1[0] != string2[0])
-                        {
-                            Console.WriteLine(string1[0] +  "-" + string2[0] + "." + string2[1] + "." + string2[2]);
-                        }
-                        else
-                        {
-                            Console.WriteLine(string2[0] + "." + string2[1] + "." + string2[2]);
-                        }
-
-                    }
-                }
-            }
-            catch
-            {
-                Console.WriteLine("Error");
-            }
-           
-          
         }
     }
 }
